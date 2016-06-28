08fad510880a8c0b7a98990450fca9d0b3d4d18d
Minh Nguyen <minh.nguyen@asnet.com.vn>
2016-06-28 17:39:24 +0700

    http - wiki search

--- sample/app/app.component.ts
+++ sample/app/app.component.ts
@@ -3,27 +3,54 @@ import { Component } from '@angular/core';
 import { HighlightComponent } from './directives/highlight-directive/highlight.component';
 import { UnlessComponent } from './directives/unless-directive/unless.component';
 import { StyleComponent } from './component-styles/component-style.component';
+
 import { OnchangeParentComponent } from './lifecycle/onchange/onchange-parent.component';
 import { AfterViewComponent } from './lifecycle/afterview/after-view.component';
 import { AfterContentParentComponent } from './lifecycle/aftercontent/after-content-parent.component';
+
+import { DatePipeComponent } from './pipes/build-in/date-pipe.component';
+import { DatePipe2Component } from './pipes/build-in/date-pipe-2.component';
+import { PowerBoostCalculatorComponent } from './pipes/dev/power-booster.component';
+
+import { WikiComponent } from './http/wiki/wikipedia.component';
+
+// Add the RxJS Observable operators we need in this app.
+import './rxjs-operators';
 
 @Component({
   selector: 'my-app',
   template:
     // "<attribute-directive></attribute-directive>"
     // "<unless-directive></unless-directive>"
+
     // "<component-style></component-style>"
+
     // "<onchange-parent></onchange-parent>"
     // "<after-view></after-view>"
-    "<after-content-parent></after-content-parent>"
+    // "<after-content-parent></after-content-parent>"
+
+    // "<hero-birthday></hero-birthday>"
+    // "<hero-birthday-2></hero-birthday-2>"
+
+    // "<power-boost-calculator></power-boost-calculator>"
+    "<my-wiki></my-wiki>"
   ,
   directives: [
     HighlightComponent,
     UnlessComponent,
+
     StyleComponent,
+
     OnchangeParentComponent,
     AfterViewComponent,
-    AfterContentParentComponent
+    AfterContentParentComponent,
+
+    DatePipeComponent,
+    DatePipe2Component,
+
+    PowerBoostCalculatorComponent,
+
+    WikiComponent
   ]
 })
 
---
+++ sample/app/http/wiki/wikipedia.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Jsonp, URLSearchParams } from '@angular/http';
+
+@Injectable()
+export class WikipediaService {
+  constructor(private jsonp: Jsonp) {
+
+  }
+
+  search (term: string) {
+    let wikiUrl = 'http://en.wikipedia.org/w/api.php';
+    let params = new URLSearchParams();
+    params.set('search', term);
+    params.set('action', 'opensearch');
+    params.set('format', 'json');
+    params.set('callback', 'JSONP_CALLBACK');
+    // TODO: Add error handling
+    return this.jsonp
+               .get(wikiUrl, { search: params })
+               .map(request => <string[]> request.json()[1]);
+  }
+
+  search2 (term: string) {
+    let wikiUrl = 'http://en.wikipedia.org/w/api.php';
+    let queryString =
+      `?search=${term}&action=opensearch&format=json&callback=JSONP_CALLBACK`;
+
+    return this.jsonp
+               .get(wikiUrl + queryString)
+               .map(request => <string[]> request.json()[1]);
+
+  }
+}
