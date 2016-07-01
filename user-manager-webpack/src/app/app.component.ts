75408891fe5dafbed7d3639bb88de4153c9456ce
Minh Nguyen <minh.nguyen@asnet.com.vn>
2016-07-01 18:26:56 +0700

    user manager - create project

---
+++ user-manager-webpack/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'todo-app',
+  template: require('./app.component.jade'),
+})
+
+export class AppComponent {
+
+}
